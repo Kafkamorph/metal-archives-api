ee7147aa2dbb535ac7e8fc3aa0e053477803975d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-12-03 23:23:40 -0500

    Debugging asynchronous browser calls WIP
    Unfortunately Watir does not have the ability to turn redirection off.  Because of this I'm having some trouble implementing the scraper to search for a band correctly.  When you type in a band that only has one entry in the metal-archives database, the search timeline goes through three seperate hits.
    
    1: You get the search band page with the html table that initially says it's searching.
    2: There's a moment where the ajax returns something but the redirection hasn't happened yet.
    3: Redirection to the band's profile page.
    
    Because I cannot figure out a way to see if any divs have been removed or changed in the moment for step two, I'm not sure how to work around this besides making watir just timeout for a few seconds while metal-archives does its magic.  That's obviously not a very efficient route to take, it feels really hacky.

--- app/controllers/band_name_controller.rb
+++ app/controllers/band_name_controller.rb
@@ -1,7 +1,27 @@
+require 'watir-webdriver'
+require 'open-uri'
+
 class BandNameController < ApplicationController
 
   def index
-    render :text => "#{params[:band_name]}"
+    search_url = "http://www.metal-archives.com/search?searchString=#{params[:band_name]}&type=band_name"
+
+    browser = Watir::Browser.new :phantomjs
+    browser.goto(search_url)
+
+    ajax_call_hidden = browser.div(:class => "dataTables_processing").style("visibility" == "hidden")
+
+
+    Watir::Wait.until { ajax_call_hidden }
+    browser.driver.manage.timeouts.implicit_wait = 1
+    site = Nokogiri::HTML(browser.html)
+    band_content = browser.div(:id => "band_content")
+    if band_content.present?
+      binding.pry
+    else
+      band_table_rows = site.css("tr").to_a[1..-1]
+      binding.pry
+    end
   end
 
 end
