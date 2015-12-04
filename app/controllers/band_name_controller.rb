aa8718914072385aefbb4cd2d20e6a112c02713a
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-12-04 15:14:17 -0500

    Extracting band profile link from page WIP
    I've figured out how to pull out the band link when the band table rows populates properly.  But I've now run into a bug where the page STILL isn't fully rendering and I can't target the correct elements.

--- app/controllers/band_name_controller.rb
+++ app/controllers/band_name_controller.rb
@@ -9,19 +9,24 @@ class BandNameController < ApplicationController
     browser = Watir::Browser.new :phantomjs
     browser.goto(search_url)
 
-    ajax_call_hidden = browser.div(:class => "dataTables_processing").style("visibility" == "hidden")
+    ajax_call_hidden = browser.div(class: "dataTables_processing").style("visibility" == "hidden")
 
 
     Watir::Wait.until { ajax_call_hidden }
-    browser.driver.manage.timeouts.implicit_wait = 1
+    browser.driver.manage.timeouts.implicit_wait = 3
     site = Nokogiri::HTML(browser.html)
-    band_content = browser.div(:id => "band_content")
+    band_content = browser.div(id: "band_content")
     if band_content.present?
       binding.pry
     else
       band_table_rows = site.css("tr").to_a[1..-1]
       binding.pry
+      band_links = band_table_rows.each_with_object([]) do |table_row, ary|
+        profile_link = table_row.elements[0].elements[0].attributes["href"].value
+        ary << profile_link
+        binding.pry
+      end
+      binding.pry
     end
   end
-
 end
