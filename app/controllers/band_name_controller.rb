214d779557861ddbba6e49b8d941e3140a6dae6b
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-12-07 18:26:17 -0500

    Fixed unknown object bug
    Fixed the bug.  Added a rescue to catch the exception watir was throwing and then reload.  Need to start implementing ruby objects for the scraped data to create.

--- app/controllers/band_name_controller.rb
+++ app/controllers/band_name_controller.rb
@@ -10,23 +10,23 @@ class BandNameController < ApplicationController
     browser.goto(search_url)
 
 
-    # binding.pry
-    Watir::Wait.until { browser.div(id: "band_content").present? || browser.table(:id, 'searchResults').to_a[1..-1].length > 1 }
-    site = Nokogiri::HTML(browser.html)
-    binding.pry
+    found = false
+    count = 0
+    until found || count > 4
+      begin
+        count += 1
+        Watir::Wait.until { browser.div(id: "band_content").present? || browser.table(:id, 'searchResults').to_a[1..-1].length > 1 }
+        found = true
+       rescue Watir::Exception::UnknownObjectException
+        puts "Saw error"
+        sleep 1
+       end
+    end
+
+    if found
+      site = Nokogiri::HTML(browser.html)
+      binding.pry
+    end
     browser.close
-    # band_content = browser.div(id: "band_content")
-    # if band_content.present?
-    #   binding.pry
-    # else
-    #   band_table_rows = site.css("tr").to_a[1..-1]
-    #   binding.pry
-    #   band_links = band_table_rows.each_with_object([]) do |table_row, ary|
-    #     profile_link = table_row.elements[0].elements[0].attributes["href"].value
-    #     ary << profile_link
-    #     binding.pry
-    #   end
-    #   binding.pry
-    # end
   end
 end
