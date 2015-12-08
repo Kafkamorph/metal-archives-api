a2ec2ad8d1fdbc1621967b072eea950c586bbe0c
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-12-08 18:38:02 -0500

    Add default values to fetch method while initializing band class

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,19 +1,19 @@
 class Band
 
   def initialize(args = {})
-    @band_name = args.fetch(:band_name)
-    @country_of_origin = args.fetch(:country_of_origin)
-    @location = args.fetch(:location)
-    @status = args.fetch(:status)
-    @formed_in = args.fetch(:formed_in)
-    @genre = args.fetch(:genre)
-    @lyrical_themes = args.fetch(:lyrical_themes)
-    @current_label = args.fetch(:current_label)
-    @years_active = args.fetch(:years_active)
-    @band_name_img = args.fetch(:band_name_img)
-    @band_img = args.fetch(:band_img)
-    @albums = args.fetch(:albums)
-    @members = args.fetch(:members)
+    @band_name = args.fetch(:band_name) {|e| "No #{e} found"}
+    @country_of_origin = args.fetch(:country_of_origin) {|e| "No #{e} found"}
+    @location = args.fetch(:location) {|e| "No #{e} found"}
+    @status = args.fetch(:status) {|e| "No #{e} found"}
+    @formed_in = args.fetch(:formed_in) {|e| "No #{e} found"}
+    @genre = args.fetch(:genre) {|e| "No #{e} found"}
+    @lyrical_themes = args.fetch(:lyrical_themes) {|e| "No #{e} found"}
+    @current_label = args.fetch(:current_label) {|e| "No #{e} found"}
+    @years_active = args.fetch(:years_active) {|e| "No #{e} found"}
+    @band_name_img = args.fetch(:band_name_img) {|e| "No #{e} found"}
+    @band_img = args.fetch(:band_img) {|e| "No #{e} found"}
+    @albums = args.fetch(:albums) {|e| "No #{e} found"}
+    @members = args.fetch(:members) {|e| "No #{e} found"}
   end
 
 end
