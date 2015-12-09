3e1a6b9994b79b4d9e369adeb31c2b87b496410e
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-12-09 02:08:22 -0500

    Add attr_readers to band class

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,5 @@
 class Band
+  attr_reader :band_name, :country_of_origin, :location, :status, :formed_in, :genre, :lyrical_themes, :current_label, :years_active, :band_name_img, :band_img, :albums, :members
 
   def initialize(args = {})
     @band_name = args.fetch(:band_name) {|e| "No #{e} found"}
