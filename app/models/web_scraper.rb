4a607b989a74d91166cc963c18465b43650081b4
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-12-09 02:08:37 -0500

    Fix css being targeted for albums

--- app/models/web_scraper.rb
+++ app/models/web_scraper.rb
@@ -2,6 +2,7 @@ class WebScraper
 
   def self.scrape_band_page(html)
     site = Nokogiri::HTML(html)
+    # Collect band statistics from page
     keys = site.css("div#band_stats dt").map {|e| e.text.chop.squish.downcase.tr(" ", "_").to_sym}
     values = site.css("div#band_stats dd").map {|e| e.text.squish}
     band_attribs = Hash[keys.zip values]
@@ -9,14 +10,14 @@ class WebScraper
     band_attribs[:band_name_img] = site.css("a#logo")[0].attributes["href"].value
     band_attribs[:band_img] = site.css("a#photo")[0].attributes["href"].value
 
-    # binding.pry
-    # This div#ui-tabs-5 needs to be targeted in a different way, since the id changes
-    album_header = site.css("div#band_disco div:nth-child(2) tr")[0].css("th").map {|e| e.text.downcase.to_sym}
-
-    album_data = site.css("div#band_disco div:nth-child(2) tr")[1..-1].map {|e| e.css("td").map {|el| el.text.squish}}
-
+    # Collect albums from page
+    album_header = site.css("div#band_disco div[role]:nth-child(2) tr")[0].css("th").map {|e| e.text.downcase.to_sym}
+    album_data = site.css("div#band_disco div[role]:nth-child(2) tr")[1..-1].map {|e| e.css("td").map {|el| el.text.squish}}
     album_hashes = album_data.map {|e| Hash[album_header.zip(e)]}
     band_attribs[:albums] = album_hashes.map {|album| Album.new(album)}
+
+    # Collect members from page
+    binding.pry
     band_attribs
   end
 
