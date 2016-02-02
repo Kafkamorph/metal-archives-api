53862d4943a881cb0238ab1c022d1b50f9027ed9
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-02-02 17:25:12 -0500

    Fix no associated band bug
    Add index while iterating through members, and check if previous member chunk had bands, and then inject the no associated band string into the array appropriately.

--- app/models/web_scraper.rb
+++ app/models/web_scraper.rb
@@ -20,13 +20,15 @@ class WebScraper
         member.shift.text.squish
       end
       chunked_members_array.each_with_index do |member_chunk_by_status, index|
-        member_chunk_by_status.each do |member|
-          # binding.pry
+        member_chunk_by_status.each_with_index do |member, member_index|
           if member.attributes['class'].value == 'lineupRow'
             member_data << member_statuses[index]
             member_data << member.at_css('a').attributes['href'].value[29..-1]
             member_data << member.at_css('a').text
             member_data << member.css('td')[-1].text.squish
+            if member_chunk_by_status[member_index - 1].attributes['class'].value == 'lineupRow'
+              member_data.insert(-5, "No Associated Bands")
+            end
           elsif member.attributes['class'].value == 'lineupBandsRow'
             member_data << self.create_associated_bands(member)
           end
@@ -37,29 +39,31 @@ class WebScraper
       chunked_members_array = site.xpath("//div[@id='band_members']//tr").slice_when { |i, j| j.attributes['class'].value == "lineupRow" }.to_a
       member_status = site.at_xpath("//div[@id='band_members']//a").text
       chunked_members_array.each do |member_chunk|
-        member_chunk.each do |member|
+        member_chunk.each_with_index do |member, index|
           if member.attributes['class'].value == 'lineupRow'
             member_data << member_status
             member_data << member.at_css('a').attributes['href'].value[29..-1]
             member_data << member.at_css('a').text
             member_data << member.css('td')[-1].text.squish
+            if member_chunk[index - 1].attributes['class'].value == 'lineupRow'
+              member_data.insert(-5, "No Associated Bands")
+            end
           elsif member.attributes['class'].value == 'lineupBandsRow'
             member_data << self.create_associated_bands(member)
           end
         end
       end
     end
-    # member_keys.pop
     member_hashes = member_data.each_slice(5).map { |e| Hash[member_keys.zip(e)] }
     band_attribs[:members] = member_hashes.map { |member| Member.new(member) }
     band_attribs
   end
 
   def self.create_associated_bands(member)
-    associated_bands_array = member.text.squish.split(/:(.+)/)[1].split(',').map {|e| e[1..-1]}
     pattern = /\A(ex-)?(.*?)?(\(live\))?\z/
-    final_ary = []
-    associated_bands_array.each do |band_name|
+    associated_bands_array = member.text.squish.split(/:(.+)/)[1].split(',').map {|e| e[1..-1]}
+
+    associated_bands_array.each_with_object([]) do |band_name, ary|
       regexed_array = pattern.match(band_name).captures.map!(&:to_s)
       associated_band_attribs = Hash.new
       associated_band_attribs[:band_name] = regexed_array[1].squish
@@ -68,24 +72,9 @@ class WebScraper
         associated_band_attribs[:band_id] = band_with_links[0].attributes['href'].value[29..-1]
       end
       associated_band_attribs[:member_relationship_to_band] = regexed_array[0] + regexed_array[2]
-      final_ary << Band.new(associated_band_attribs)
-      # binding.pry
-
+      ary << Band.new(associated_band_attribs)
     end
-    final_ary
   end
-    # member.css('a').each do |band_link|
-    #   associated_bands_array.delete_if do |band_name|
-    #     if band_name.include? band_link.text
-    #       final_ary << Band.new(band_name: band_name, band_id: band_link.attributes['href'].value[29..-1])
-    #     else
-    #       final_ary << Band.new(band_name: band_name)
-    #       binding.pry
-    #       # true
-    #     end
-    #     true
-    #   end
-    # end
 
   def self.scrape_search_page(browser)
     site = Nokogiri::HTML(browser.html)
