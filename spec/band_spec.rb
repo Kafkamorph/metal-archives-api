6482bad3ac4cec269823df12138fdce55498526f
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-02-03 20:06:33 -0500

    Add spec for get band
    Add the most basic spec, feeding the controller the band show route for the band "Ancestors".  This is checking that the return status number is 200 and the json object has the correct name and id.

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 describe 'Bands API', type: :request do
   it 'sends a band' do
+    band_name = "Ancestors"
+    band_id = "93304"
 
+    get "/bands/#{band_name}/#{band_id}", nil \
 
+    expect(response).to have_http_status(:success)
+
+    json = JSON.parse(response.body)
+    expect(json["band_name"]).to eq band_name
+    expect(json["band_id"]).to eq "/bands/#{band_name}/#{band_id}"
   end
 end
