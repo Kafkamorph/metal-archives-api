055a7f4e379d73eea44b48ee184750da817e3486
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-02-10 18:34:52 -0500

    Remove valid factory test and check ancestors api call

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 describe 'Bands API', type: :request do
-  it 'sends a band' do
-    band_name = "Ancestors"
-    band_id = "93304"
-
-    get "/bands/#{band_name}/#{band_id}", nil \
+  it 'sends ancestors band' do
+    ancestors = FactoryGirl.build(:ancestors_band)
+    get "#{ancestors.band_id}", nil \
 
     expect(response).to have_http_status(:success)
 
     json = JSON.parse(response.body)
-    expect(json["band_name"]).to eq band_name
-    expect(json["band_id"]).to eq "/bands/#{band_name}/#{band_id}"
+    expect(json["band_name"]).to eq ancestors.band_name
+    expect(json["band_id"]).to eq ancestors.band_id
+    expect(json["members"][0]["name"]).to eq "Mark McCoy"
   end
 end
