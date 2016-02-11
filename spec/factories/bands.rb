459cb39917515140af561493d9c70af594da669b
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-02-10 19:53:45 -0500

    Add mutilation rites band factory

--- spec/factories/bands.rb
+++ spec/factories/bands.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
         band_name "Ancestors"
         band_id "/bands/Ancestors/93304"
     end
+    factory :mutilation_rites_band do
+        band_name "Mutilation Rites"
+        band_id "/bands/Mutilation_Rites/3540306985"
+    end
   end
 end
