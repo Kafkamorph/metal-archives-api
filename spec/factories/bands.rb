1ccd03bfdc09a2e215fe0a13b13e29dd680b2126
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-02-10 20:04:28 -0500

    Add voivod factory

--- spec/factories/bands.rb
+++ spec/factories/bands.rb
@@ -8,5 +8,9 @@ FactoryGirl.define do
         band_name "Mutilation Rites"
         band_id "/bands/Mutilation_Rites/3540306985"
     end
+    factory :voivod_band do
+        band_name "Voivod"
+        band_id "/bands/Voivod/115"
+    end
   end
 end
